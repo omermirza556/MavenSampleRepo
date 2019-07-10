fe4c5dfa9ddd861187a3e48e216f1f640c580b0e
185879 <185879@USREMWFHFV0P9H2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-07-10 11:50:33 -0500

    second

--- MavenSample/src/main/java/com/first/MathOpperation.java
+++ MavenSample/src/main/java/com/first/MathOpperation.java
@@ -11,12 +11,17 @@ public class MathOpperation {
 	public int mul(int a , int b) {
 		return a * b;
 	}
+	public int div(int a , int b) {
+		return a / b;
+	}
 	
 	public static void main(String[] args) {
 		MathOpperation mo = new MathOpperation();
 		System.out.println("" + mo.add(5, 5));
 		System.out.println("" + mo.sub(5, 5));
 		System.out.println("" + mo.mul(5, 5));
+
+		System.out.println("" + mo.div(5, 5));
 	}
 
 }
